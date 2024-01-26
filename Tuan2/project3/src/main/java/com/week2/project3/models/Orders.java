8bb45a092d0f83e34f0e7f64c9ca1e68ff865b20
Taka472 <121855825+Taka472@users.noreply.github.com>
2024-01-26 15:31:24 +0700

    Add files via upload

---
+++ Tuan2/project3/src/main/java/com/week2/project3/models/Orders.java
@@ -0,0 +1,40 @@
+package com.week2.project3.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Orders {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private double price;
+	
+	
+	public Orders() {
+
+	}
+	public Orders(double price) {
+		this.price = price;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+		return "Orders [id=" + id + ", price=" + price + "]";
+	}
+	
+	
+}
