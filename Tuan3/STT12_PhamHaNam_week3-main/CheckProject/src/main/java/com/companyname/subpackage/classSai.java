a579194f7c676e1ea42dd4d7e806525b6202d1c7
Huy Trần Lê <tranlehuy0407@gmail.com>
2024-03-10 19:21:23 +0700

    pushing all week

---
+++ Tuan3/STT12_PhamHaNam_week3-main/CheckProject/src/main/java/com/companyname/subpackage/classSai.java
@@ -0,0 +1,19 @@
+package com.company.subpackage;
+
+public class classSai {
+    private int FieldSai;
+    // constant
+    private static final int CONSTANT_WRONG = 0;
+
+    public classSai(int myField) {
+        this.FieldSai = myField;
+    }
+
+    public int getFieldSai() {
+        return FieldSai;
+    }
+
+    public void setFieldSai(int myField) {
+        this.FieldSai = myField;
+    }
+}
