0b21514fd6cc2d7461527f3994d1119ae2c98297
Taka472 <121855825+Taka472@users.noreply.github.com>
2024-03-22 17:17:57 +0700

    Add files via upload

---
+++ Eureka/registry/src/test/java/iuh/eureka/registry/RegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.eureka.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
